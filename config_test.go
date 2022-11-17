a3fe5286d12d977e58019fc30f94e3338fcaaf5a
Raphaël Ricordeau <raphael.ricordeau@ac-dijon.fr>
2022-11-17 19:23:04 +0000

    first test

--- config_test.go
+++ config_test.go
@@ -1,7 +1,39 @@
 package sushi
 
-import "testing"
+import (
+	"runtime"
+	"testing"
 
-func TestConfig(t *testing.T) {
+	"gopkg.in/yaml.v3"
+)
+
+func print_test_header() {
+	println("************************************************************")
+	pc, _, _, _ := runtime.Caller(1)
+	fn := runtime.FuncForPC(pc)
+	println("** " + fn.Name())
+	println("************************************************************")
+}
+
+func Test_LoadConfigBytes(t *testing.T) {
+	print_test_header()
+	println("chargement de la configuration")
+	b, err := LoadConfigBytes("sushi.sample.yml")
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	var c []*Node
+
+	err = yaml.Unmarshal(b, &c)
+	if err != nil {
+		t.Error(err)
+	}
+
+	println(c[0].Name)
+	if c[0].Name != "server with nothing" {
+		t.Error("La valeur trouvé dans la configuration n'est pas celle attendu")
+	}
 
 }
